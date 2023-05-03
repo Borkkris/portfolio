d355d86a912a696017db4aeb130a79283244db77
Borkkris <Borkkris@users.noreply.github.com>
2023-05-03 12:44:37 +0200

    updated techstack

--- src/components/TechstackComponent.js
+++ src/components/TechstackComponent.js
@@ -8,26 +8,23 @@ import Nodejs_logo from '../assets/TechStack/Nodejs.png';
 import MongoDB_logo from '../assets/TechStack/MongoDB.png';
 import Firebase_logo from '../assets/TechStack/Firebase.png';
 import Reactjs_logo from '../assets/TechStack/Reactjs.png';
+import Angular_logo from '../assets/TechStack/Angular.png';
 import Expo_logo from '../assets/TechStack/Expo.png';
 
 const TechstackComponent = () => {
   return (
-    <div id='techstack' className='flex overflow-hidden p-5'>
-      <div className='animate-marquee flex'>
-        <img src={HTML_logo} alt="HTML5" className='mr-10 h-28' />
-        <img src={CSS_logo} alt="CSS3" className='mr-10 h-28' />
-        <img src={JS_logo} alt="JS" className='mr-10 h-28' />
-        <img src={Tailwind_logo} alt="Tailwindcss" className='mr-10 h-28' />
-        <img src={Nodejs_logo} alt="Nodejs" className='mr-10 h-28' />
-        <img src={MongoDB_logo} alt="MongoDB" className='mr-10 h-28' />
-        <img src={Firebase_logo} alt="Firebase" className='mr-14 h-28' />
-        <img src={Reactjs_logo} alt="Reactjs" className=' h-28' />
-        <img src={Expo_logo} alt="Expo" className=' h-28' />
-        <img src={HTML_logo} alt="HTML5" className='mr-10 h-28' />
-        <img src={CSS_logo} alt="CSS3" className='mr-10 h-28' />
-        <img src={JS_logo} alt="JS" className='mr-10 h-28' />
-        <img src={Tailwind_logo} alt="Tailwindcss" className='mr-10 h-28' />
-        <img src={Nodejs_logo} alt="Nodejs" className='mr-10 h-28' />
+    <div id='techstack' className='p-5'>
+      <div className='flex flex-wrap justify-evenly'>
+        <img src={HTML_logo} alt="HTML5" className='h-10 mx-2 my-1'/>
+        <img src={CSS_logo} alt="CSS3" className='h-10 mx-2 my-1'/>
+        <img src={JS_logo} alt="JS" className='h-10 mx-2 my-1'/>
+        <img src={Tailwind_logo} alt="Tailwindcss" className='h-10 mx-2 my-1'/>
+        <img src={Nodejs_logo} alt="Nodejs" className='h-10 mx-2 my-1'/>
+        <img src={MongoDB_logo} alt="MongoDB" className='h-10 mx-2 my-1'/>
+        <img src={Firebase_logo} alt="Firebase" className='h-10 mx-2 my-1'/>
+        <img src={Reactjs_logo} alt="Reactjs" className='h-10 mx-2 my-1'/>
+        <img src={Angular_logo} alt="Angular" className='h-10 mx-2 my-1'/>
+        <img src={Expo_logo} alt="Expo" className='h-10 mx-2 my-1'/>
       </div>
     </div>
   )
