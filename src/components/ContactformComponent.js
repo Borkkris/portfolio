87bcb3dcb97aeec9738bab158ae77a20e4d133d6
Borkkris <Borkkris@users.noreply.github.com>
2023-05-08 20:01:39 +0200

    - implemented the contactform - minor style changes

--- src/components/ContactformComponent.js
+++ src/components/ContactformComponent.js
@@ -1,52 +1,137 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const ContactformComponent = () => {
+
+  const [message, setMessage] = useState('');
+  const MAX_MESSAGE_LENGTH = 200;
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  }
+
+  const messageLength = message.length;
+  const messageLengthString = `${messageLength}/${MAX_MESSAGE_LENGTH}`;
+
+  const maxMessageLength = () => {
+    return (messageLength >= MAX_MESSAGE_LENGTH)
+  }
+
   return (
-    <div id='contactform'>
-      <form className='w-full md:w-1/2 max-w-lg'>
-        <div className='flex flex-wrap mb-6'>
-          <div className='w-full px-3'>
-            <label className='block uppercase tracking-wide'>
-              First Name
-            </label>
-            <input className='bg-slate-200 rounded appearance-none' type='text' name='name' placeholder="Jane" />
+    <div className='py-20' id='contactform'>
+
+      <h1 className='font-extrabold text-xl justify-center flex'>Contact</h1>
+
+      <div className='flex justify-center'>
+        <form className='w-full max-w-lg py-6'>
+
+          <div className='flex flex-wrap mb-6'>
+            <div className='w-full md:w-1/2 mb-6 md:mb-0 px-3'>
+              <label className='block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2 '>
+                First Name
+              </label>
+              <input 
+                className='appearance-none block w-full bg-gray-200 text-gray-700 
+                          border border-gray-200 rounded py-3 px-4 mb-3 leading-tight 
+                          focus:outline-none focus:bg-white focus:border-gray-500' 
+                type='text' 
+                name='name' 
+                placeholder='Jane' />
+            </div>
+            <div className='w-full md:w-1/2 mb-6 md:mb-0 px-3'>
+              <label className='block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2 '>
+                Last Name
+              </label>
+              <input 
+                className='appearance-none block w-full bg-gray-200 text-gray-700 
+                          border border-gray-200 rounded py-3 px-4 mb-3 leading-tight 
+                          focus:outline-none focus:bg-white focus:border-gray-500' 
+                type='text' 
+                name='lastname' 
+                placeholder='Doe' 
+              />
+            </div>
           </div>
-          <div className='w-full md:w-1/2 px-3'>
-            <label className='block uppercase tracking-wide'>
-              Last Name
-            </label>
-            <input className='w-30 h5 bg-slate-200 rounded' type='text' name='lastname' placeholder="Doe" />
+
+          <div className='flex flex-wrap mb-6'>
+            <div className='w-full md:w-1/2 mb-6 md:mb-0 px-3'>
+              <label className='block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2 '>
+                Email
+              </label>
+              <input 
+                className='appearance-none block w-full bg-gray-200 text-gray-700 
+                          border border-gray-200 rounded py-3 px-4 mb-3 leading-tight 
+                          focus:outline-none focus:bg-white focus:border-gray-500' 
+                type='email' 
+                name='Email' 
+                placeholder='janedoe@gmail.com' 
+              />
+            </div>
+            <div className='w-full md:w-1/2 mb-6 md:mb-0 px-3'>
+              <label className='block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2 '>
+                Telephone
+              </label>
+              <input 
+                className='appearance-none block w-full bg-gray-200 text-gray-700 
+                          border border-gray-200 rounded py-3 px-4 mb-3 leading-tight 
+                          focus:outline-none focus:bg-white focus:border-gray-500' 
+                type='tel' 
+                name='telephone' 
+                placeholder='1234567890' 
+              />
+            </div>
           </div>
-        </div>
-        <div className='flex flex-wrap mb-12'>
-          <div className='w-full md:w-1/2 px-3'>
-            <label className='block uppercase tracking-wide'>
-              Email
-            </label>
-            <input className='w-30 h5 bg-slate-200 rounded' type='email' name='Email' placeholder="janedoe@gmail.com" />
+
+          <div className='flex flex-wrap'>
+            <div className='w-full md:w-1/2 mb-6 md:mb-0 px-3'>
+              <label className='block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2 '>
+                Subject
+              </label>
+              <input 
+                className='appearance-none block w-full bg-gray-200 text-gray-700 
+                          border border-gray-200 rounded py-3 px-4 mb-3 leading-tight 
+                          focus:outline-none focus:bg-white focus:border-gray-500' 
+                type='text' 
+                name='subject' 
+              />
+            </div>
           </div>
-        </div>
-        <div className='flex flex-wrap'>
-          <div className='w-full md:w-1/2 px-3 mb-3'>
-            <label className='block uppercase tracking-wide'>
-              Subject
-            </label>
-            <input className='w-30 h5 bg-slate-200 rounded' type='text' name='subject' />
+
+          <div className='flex flex-wrap relative'>
+            <div className='w-full mb-6 md:mb-0 px-3'>
+              <label className='block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2'>
+                Message
+              </label>
+              <textarea 
+                className='appearance-none block flex-grow h-full w-full bg-gray-200 text-gray-700 
+                          border border-gray-200 rounded py-3 px-4 mb-3 leading-tight 
+                          focus:outline-none focus:bg-white focus:border-gray-500' 
+                name='message' 
+                placeholder='type here...'
+                maxlength='200'
+                value={message}
+                onChange={handleChange}
+                maxLength={MAX_MESSAGE_LENGTH}
+              />
+              {maxMessageLength()? <p className='pb-6 absolute right-2 -mt-3 text-red-600'>200/200</p> : <div className='pb-6 absolute right-2 -mt-3'>{messageLengthString}</div>}
+              
+            </div>
           </div>
-          <div className='w-full md:w-1/2 px-3'>
-            <label className='block uppercase tracking-wide'>
-              Message
-            </label>
-            <textarea className='h5 bg-slate-200 rounded' name='message' placeholder="type here..."/>
+
+          <div className='px-3 pt-12'>
+            <button 
+              className='shadow bg-sky-500 hover:bg-sky-600 focus:shadow-outline 
+                        focus:outline-none text-white font-bold py-2 px-4 rounded' 
+              type='button'
+              onClick={()=>{}}
+            >
+              send
+            </button>
           </div>
-        </div>
-        <div className='px-3'>
-          <button className="shadow bg-sky-500 hover:bg-sky-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-            send
-          </button>
-        </div>
-      </form>
+          
+        </form>
+      </div>
+      
     </div>
   )
 }
--- src/components/NavigationComponent.js
+++ src/components/NavigationComponent.js
@@ -45,55 +45,20 @@ const NavigationComponent = () => {
                         const element = document.getElementById("contactform");
                         element.scrollIntoView({ behavior: "smooth" });
                     }}>
-                    <p>Contact me</p>
+                    <p>Contact</p>
                 </button>
 
                 <button 
                     id='dropdownMenuIconHorizontalButton' 
-                    data-dropdown-toggle='dropdownDotsHorizontal' 
                     class='inline-flex items-center p-2 text-sm 
                         font-medium text-center text- bg-white 
                         rounded-lg hover:bg-gray-100 focus:ring-1 
                         focus:outline-none dark:text-black focus:ring-black 
                         dark:hover:bg-gray dark:bg-gray dark:focus:ring-gray-600' 
                         type='button'> 
-                    <svg 
-                        class='w-6 h-6' 
-                        aria-hidden='true' 
-                        fill='currentColor' 
-                        viewBox='0 0 20 20' 
-                        xmlns='http://www.w3.org/2000/svg'>
-                        <path d='M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z'></path>
-                    </svg>
                 </button>
-                {/* <!-- Dropdown menu --> */}
-                <div id="dropdownDotsHorizontal" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconHorizontalButton">
-                    <li>
-                        <a href="{experiences}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Experiences</a>
-                    </li>
-                    <li>
-                        <a href="{projects}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Projects</a>
-                    </li>
-                    <li>
-                        <a href="{techstack}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Techstack</a>
-                    </li>
-                    <li>
-                        <a href="{contactform}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Contact me</a>
-                    </li>
-                    </ul>
-                    <div class="py-2">
-                    </div>
-                </div>
             </div>
         </div>
-        {/* <div>
-            <img 
-                alt="header_pic_kieroń" 
-                src={headerPic} 
-                className='w-full object-contain'
-            />
-        </div> */}
     </div>
 
   )
