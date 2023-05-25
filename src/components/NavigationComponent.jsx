f244605de11bb9483e14a82c7b39a0a963c07a6e
Borkkris <Borkkris@users.noreply.github.com>
2023-05-25 18:27:03 +0200

    minor style changes to the img

--- src/components/NavigationComponent.jsx
+++ src/components/NavigationComponent.jsx
@@ -6,19 +6,19 @@ const NavigationComponent = () => {
   return (
 
     <div style={{backgroundColor:'#2A403D'}}>
-        <div style={{backgroundColor:'#2A403D'}} className='flex fixed font-extrabold border-b w-full h-20 p-0 sm:p-3 z-10'>
-            <div className="border-r-2 px-3 flex justify-start">
+        <div style={{backgroundColor:'#2A403D'}} className='flex fixed font-extrabold border-b w-full h-24 z-50'>
+            <div className="border-r-2 px-3 flex justify-between">
                 <img 
                     src={christian_bork_avatar} alt='christian' 
-                    className='object-cover object-top rounded-full sm:left-0 p-3' />
+                    className='flex object-cover object-top max-h-full max-w-full rounded-full sm:left-0 p-3'/>
                 <div className='flex items-center left-2 sm:m-0 m-3'>
-                    <p className='tracking-wider sm:m-0 m-3 text-xs sm:text-sm md:text-sm lg:text-xl xl:text-2xl uppercase'>Christian <span style={{color:'#748B6F'}}>Bork</span></p>
+                    <p className='tracking-wider text-2xl uppercase'>Christian <span style={{color:'#748B6F'}}>Bork</span></p>
                 </div>
             </div>
 
            
 
-            <div className='md:flex-row lg:flex-row flex flex-wrap flex-1 justify-end sm:m-0 px-5 text-xs sm:text-sm md:text-sm lg:text-xl xl:text-2xl'>
+            <div className='md:flex-row lg:flex-row flex flex-wrap flex-1 justify-end sm:m-0 px-5 text-2xl'>
                 
                 <button
                     className='hover:underline rounded-xl tracking-wide'                   
