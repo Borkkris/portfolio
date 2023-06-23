a1892b9507e38ee531c3e981efc37398227d324a
Borkkris <Borkkris@users.noreply.github.com>
2023-06-23 20:40:54 +0200

    style changes

--- src/components/IntroComponent.jsx
+++ src/components/IntroComponent.jsx
@@ -5,16 +5,16 @@ import { SiHey } from 'react-icons/si';
 const IntroComponent = () => {
 
   return (
-    <div id='intro' className='px-10 py-24 w-screen h-screen uppercase tracking-wide'>
-      <div className='flex flex-col'>
-        <h1 className='text-3xl md:text-5xl lg:text-6xl xl:text-6xl pb-6 md:pb-12 lg:pb-12 xl:pb-12 border-b md:leading-snug leading-tight'>
-            <SiHey className='w-16 h-auto text-black py-3'/> 
+    <div id='intro' className='px-3 md:px-3 lg:px-12 xl:px-16 py-24 w-screen h-max uppercase tracking-wide'>
+      <div className='text-right md:text-left lg:text-left xl:text-left flex flex-row md:flex-col lg:md:flex-col xl:md:flex-col justify-between border-b pb-3 md:pb-12 lg:pb-12 xl:pb-12'>
+        <SiHey className='w-24 h-auto text-black pr-3'/>
+        <h1 className='text-2xl md:text-5xl lg:text-6xl xl:text-6xl  md:leading-snug'> 
             Hi there,<br></br> I'm Christian,<br></br> and I'm ...
         </h1>
       </div>
 
       <div>
-          <p className="text-2xl md:text-4xl lg:text-4xl xl:text-4xl pt-6 md:pt-12 lg:pt-12 xl:pt-12 leading-relaxed md:leading-loose lg:leading-loose xl:leading-loose">
+          <p className="text-xl md:text-4xl lg:text-4xl xl:text-4xl pt-3 md:pt-12 lg:pt-12 xl:pt-12 leading-relaxed md:leading-relaxed lg:leading-loose xl:leading-loose">
               a certified <span style={{color:'black', fontWeight:'bolder'}}>full-stack web developer</span> with experience, 
               joy and passion for <span style={{color:'black', fontWeight:'bolder'}}>React</span> and other tools, 
               dedicated to crafting meticulous, customer-centric, and cutting-edge responsive products.
